169fed1c0ac073d55939858b629d5f60025dc88e
willylin207 <>
2023-12-09 13:27:31 -0800

    Simplify audioplayer

--- src/offscreen/audioplayer.js
+++ src/offscreen/audioplayer.js
@@ -1,10 +1,8 @@
-window.addEventListener("load", () => {
-    const AUDIOFILE_PATH = "../../sounds/4.wav";
+const AUDIOFILE_PATH = "../../sounds/4.wav";
 
-    chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-        if (sender.id === chrome.runtime.id && request.action === "playoffscreen") {
-            new Audio(AUDIOFILE_PATH)
-                .play();
-        }
-    });
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (sender.id === chrome.runtime.id && request.action === "playoffscreen") {
+        new Audio(AUDIOFILE_PATH)
+            .play();
+    }
 });
